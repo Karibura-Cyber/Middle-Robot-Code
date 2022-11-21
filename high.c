46028f70e21e8f0309194c8705fd25513fd12a98
Meck <76256502+Karibura-Cyber@users.noreply.github.com>
2022-11-21 16:28:52 +0700

    Update high.c

--- high.c
+++ high.c
@@ -1,38 +1,36 @@
 short i = 0;
 short j = 0;
+short a = 0;
+short b = 0;
 short state = 0;
 short color = 0;
-int l,il,ir,r,c;
+int l,il,ir,r,c,bl,br;
+
 void setup()  
 {
-    
+    keep_up();
     lcd("");    
 }
 void loop()
 {   
-    
     l = analog(0);
     il = analog(1);
     c = analog(2);
     ir = analog(3);
     r = analog(4);
     r90();
-    bk(100); sleep(70);
+    //bk(50); sleep(100);
     ao(); sleep(1);
     track();
     if(state == 1){  
         l90();
         track();
         if(state == 1){          
-            l90();
+            r90();
             track();
-            if(state == 1){  
+            if(state == 1){
                 r90();
                 track();
-                if(state == 1){  
-                    r90();
-                    track();
-                }
             }
         }
     }
@@ -68,8 +66,10 @@ void ss(int x){ao(); sleep(x);}
 
 void track(){
     state = 0;
-    for(j=0; j <= 160; j+=1){
-        lcd("%d",c);
+    lineup();
+    
+    for(j=0; j <= 150; j+=1){
+        //lcd("%d",c);
         l = analog(0);
         il = analog(1);
         c = analog(2);
@@ -77,18 +77,17 @@ void track(){
         r = analog(4);
         check();
         //if (c < 300){
-        if(ir < 300 && il < 300){
-            
+        if(c < 250 || ir < 250 && il < 250){
             state = 1;
             fd(0); sleep(100);
-            bk(100); sleep(70);
+            //bk(100); sleep(100);
             //fd(0); sleep(100);        
             break;
         }
-        if (il < 300){tl(100);}
-        else if (ir < 300){tr(100);}  
-        else if(l < 300){tr(50);}
-        else if(r < 300){tl(50);} 
+        if (il < 250){tl(100);}
+        else if (ir < 250){tr(100);}  
+        else if(l < 250){tr(50);}
+        else if(r < 250){tl(50);} 
         else {fd(50);}
 
         sleep(1);
@@ -97,14 +96,27 @@ void track(){
 
 void check(){
     color = 0;
-    if(c == 473 || c == 443 || c == 301){
+    if(c >= 295 && c <= 300){
         lcd("GREEN");
         ss(100);
-        move(300);
+        move(500);
         r180();
         lcd("");
         ss(10);
         
     }
 
+}
+
+void lineup(){
+    b = 0;
+    for(a = 0; a <= 25; a+=1){
+        bl = analog(5);
+        br = analog(7);
+        if(br < 250 && bl < 250){b = 1; lcd("FIND"); ss(100); lcd(""); break;}
+        else if(bl < 250){sr(50); sleep(10);}
+        else if(br < 250){sl(50); sleep(10);}
+        else{bk(50); sleep(1);}
+    }
+fd(50); sleep(10);
 }
